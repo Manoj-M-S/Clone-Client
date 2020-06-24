8c4b4535bcdbc612e37b57f74895d3b2b9c090ac
Manoj-M-S <manoj.makarasu@gmail.com>
2020-06-24 18:48:17 +0530

    Seventh Commit

---
+++ build/precache-manifest.734efcc2822ab7f0fc5e55b11ce0356e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02a8f60ed9ccc8ad792adfe2e55ed0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9b9bf0ebaf8a9a633fe",
+    "url": "/static/css/main.a7e63551.chunk.css"
+  },
+  {
+    "revision": "70599d55bb10cb137f5f",
+    "url": "/static/js/2.0214920c.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.0214920c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b9bf0ebaf8a9a633fe",
+    "url": "/static/js/main.da972d8d.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
